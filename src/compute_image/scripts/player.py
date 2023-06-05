d7dc11a9d6bef1603069d40c8b376ab830f1293c
Venkatesh <thecaptain@pop-os.localdomain>
2023-06-05 14:41:48 +0530

    initial commit

--- src/compute_image/scripts/player.py
+++ src/compute_image/scripts/player.py
@@ -3,29 +3,17 @@ import rospy
 import std_msgs
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
-import randimage
 import cv2
 import numpy as np
 
 pub1 = rospy.Publisher('/guess', Image, queue_size=10)
 
-
-	
-	
 def strategy():
-	global imgMsg
-	imgMsg = None
 	rospy.init_node('player_node')
 	rospy.Subscriber("/result", Image, guessCallback)
-	
-
-	
-		
+			
 def guessCallback(data):
-	global imgMsg
-	
-
-		
+	pass
 	
 if __name__ == '__main__':
 	try:
