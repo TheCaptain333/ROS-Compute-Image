5db792a4a21534386a52c1298ec47242f0ea06de
Venkatesh <thecaptain@pop-os.localdomain>
2023-06-05 15:47:37 +0530

    Updated ROS Package Template

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/thecaptain/TASKS/src
-# Build directory: /home/thecaptain/TASKS/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("compute_image")
--- build/compute_image/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/thecaptain/TASKS/src/compute_image
-# Build directory: /home/thecaptain/TASKS/build/compute_image
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
